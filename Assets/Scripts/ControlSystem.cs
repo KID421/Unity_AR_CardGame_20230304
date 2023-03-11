b2b361200aaf6b24694027027922c12c17f0da47
KID <50666207+KID421@users.noreply.github.com>
2023-03-11 11:56:17 +0800

    Update 添加控制系統

---
+++ Assets/Scripts/ControlSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    public class ControlSystem : MonoBehaviour
+    {
+        [SerializeField, Header("目標物件")]
+        private Transform target;
+
+        private void Update()
+        {
+            LookAtTarget();
+        }
+
+        /// <summary>
+        /// 面向目標
+        /// </summary>
+        private void LookAtTarget()
+        {
+            // 變形元件.面向(目標物件)
+            transform.LookAt(target);
+
+            // X 與 Z 歸零
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        }
+    }
+}
+
